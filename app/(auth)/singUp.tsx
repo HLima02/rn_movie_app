b09615de509d681c47cf98059d2cd7258647514b
HLima02 <hiago.liima02@hotmail.com>
2025-12-27 16:14:13 -0300

    Página de login

--- app/(auth)/singUp.tsx
+++ app/(auth)/singUp.tsx
@@ -1,18 +1,20 @@
 import { icons } from '@/constants/icons'
 import { images } from '@/constants/images'
-import { Link } from 'expo-router'
 import { useState } from 'react'
 import { Image, Text, TextInput, TouchableOpacity, View } from 'react-native'
 
 export default function SingUp() {
   const [email, setEmail] = useState<string>('')
+  const [password, setPassword] = useState<string>('')
+  const [isSigningUp, setIsSigningUp] = useState<boolean>(true)
+
   return (
     <View className='flex-1 bg-[#030014] items-center'>
       <Image source={images.bg} className='absolute w-full z-0 top-0 left-0'  />
 
       <View className='mt-10 w-[100%] px-5 '>
         <Image source={icons.logo} className='w-12 h-10 mt-[7rem] mb-5 mx-auto' />
-        <Text className='text-white text-5xl font-bold mt-5 mb-8 text-center'>Cadastro</Text>
+        <Text className='text-white text-5xl font-bold mt-5 mb-10 text-center'>{isSigningUp ? 'Cadastro' : 'Login'}</Text>
 
         <View className='w-full mb-8 flex-row items-center bg-[#0F0D23] rounded-full px-8 py-6'>
           <TextInput 
@@ -30,18 +32,31 @@ export default function SingUp() {
             autoCapitalize='none'
             placeholder='Senha'
             placeholderTextColor="#a8b5db"
-            value={email}
-            onChangeText={(val) => setEmail(val)}
+            value={password}
+            secureTextEntry
+            onChangeText={(val) => setPassword(val)}
             className='text-white w-full text-xl'
           />
         </View>
 
         <TouchableOpacity className='w-full mt-12 bg-[#283593] py-5 px-8 rounded-2xl '>
-          <Text className='text-white text-center text-2xl font-bold'>Cadastrar</Text>
+          <Text className='text-white text-center text-2xl font-bold'>{isSigningUp ? 'Cadastro' : 'Login'}</Text>
         </TouchableOpacity>
 
         <View className='mt-8'>
-          <Link href='/' className='text-white text-lg text-center'>Já possui uma conta? Faça o Login</Link>
+          {isSigningUp ? (
+            <TouchableOpacity
+            onPress={() => setIsSigningUp(!isSigningUp)}>
+              <Text className='text-white text-lg text-center'>Já possui uma conta? <Text className='text-[#283593]'>Faça o Login!</Text></Text>
+            </TouchableOpacity>
+          ) : (
+            <TouchableOpacity 
+            onPress={() => setIsSigningUp(!isSigningUp)}>
+              <Text className='text-white text-lg text-center'>Ainda não possui uma conta? <Text className='text-[#283593]'>Cadastre-se!</Text></Text>
+            </TouchableOpacity>
+          )
+          }
+          
         </View>
       </View>
     </View>
